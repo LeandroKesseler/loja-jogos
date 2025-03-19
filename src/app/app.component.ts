b8b9ac5ee072df1afdfee5ee1a220c473d6527f1
Leandro K <leandrokesseler@gmail.com>
2025-03-18 23:01:56 -0300

    Creating Components and Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, RodapeComponent, PainelPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componentes/painel-principal/painel-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-principal',
+  imports: [],
+  templateUrl: './painel-principal.component.html',
+  styleUrl: './painel-principal.component.css'
+})
+export class PainelPrincipalComponent {
+
+}
