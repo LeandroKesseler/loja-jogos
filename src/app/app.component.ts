f0ae36c611f533285346a815e11dd3957e58ddbb
Leandro K <leandrokesseler@gmail.com>
2025-03-20 22:45:33 -0300

    Atividade 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,38 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, MenuComponent, RodapeComponent, PainelPrincipalComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'loja-jogos';
 }
+
+const routes: Routes = [
+  { path: 'painel-principal', component: PainelPrincipalComponent },
+  { path: '', redirectTo: '/painel-principal', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+@NgModule({
+  imports: [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule
+  ],
+  })
+  export class AppModule { }
--- src/app/componentes/painel-principal/painel-principal.component.ts
+++ src/app/componentes/painel-principal/painel-principal.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProdutoService } from 'src/app/servicos/produto.service';
 
 @Component({
   selector: 'app-painel-principal',
-  imports: [],
   templateUrl: './painel-principal.component.html',
-  styleUrl: './painel-principal.component.css'
+  styleUrls: ['./painel-principal.component.css']
 })
-export class PainelPrincipalComponent {
+export class PainelPrincipalComponent implements OnInit {
+  produtos: any[] = [];
 
+  constructor(private produtoService: ProdutoService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+
+  listarProdutos(): void {
+    this.produtoService.obterProdutos().subscribe({
+      next: (dados: any[]) => {
+        this.produtos = dados;
+      },
+      error: (erro: any) => {
+        console.error('Erro ao obter produtos:', erro);
+      }
+    });
+  }
+
+  excluirProduto(id: number): void {
+    if (confirm('Deseja realmente excluir este produto?')) {
+      this.produtoService.deletarProduto(id).subscribe({
+        next: () => {
+          alert('Produto excluído com sucesso!');
+          this.listarProdutos();
+        },
+        error: (erro: any) => {
+          console.error('Erro ao excluir produto:', erro);
+        }
+      });
+    }
+  }
+
+  editarProduto(id: number): void {
+    this.router.navigate(['/cadastro-produto', id]);
+  }
 }
